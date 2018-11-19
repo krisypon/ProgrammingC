7cb2081d36be1f22fd1b96db7338da9e0e2f6f1b
krisypon <31922131+krisypon@users.noreply.github.com>
2018-11-19 21:34:56 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: user
+ *
+ * Created on November 19, 2018, 6:48 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
