fd490b6a8e8699aa3b4cc19b03cf09a6fcdf0532
Kristine <31922131+krisypon@users.noreply.github.com>
2018-12-02 23:41:47 +0200

    Create C77main.c

---
+++ C77main.c
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   C77main.c
+ * Author: name
+ *
+ * Created on November 29, 2018, 11:41 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+void functionName()
+{
+    ... .. ...
+    ... .. ...
+}
+
+int main()
+{
+    ... .. ...
+    ... .. ...
+
+    functionName();
+    
+    ... .. ...
+    ... .. ...
+}
